ca68c471beec5d2bf66e50956caa1df52742059d
Pankaj <pc620042@gmail.com>
2025-01-06 11:58:51 +0530

    Adjust the styles to make the product grid look good

--- script/quickCart.js
+++ script/quickCart.js
@@ -1,3 +1,8 @@
 import { renderHeader } from "./header.js";
 
 renderHeader();
+
+document.getElementById("add-to-cart").addEventListener("click",  () => {
+    const addedTOCart = document.getElementById("added-to-cart");
+    addedTOCart.style.opacity = 1;
+});
