0d6ddbc258c36d88ffb25fcf0de787e045ee3a43
Pankaj <pc620042@gmail.com>
2025-01-13 16:42:45 +0530

    Make the header responsive

--- script/header.js
+++ script/header.js
@@ -2,7 +2,7 @@ export function renderHeader() {
   const header = `
     <div class="left-section">
     <div class="brand-container">
-      <a href="/quickCart.html">
+      <a href="index.html">
         <img src="logo/QuickCart.png" alt="brand logo" class="logo brand-logo-small">
         <img src="logo/QuickCart-big.png" alt="" class="logo brand-logo-big">
       </a>
@@ -20,18 +20,28 @@ export function renderHeader() {
     </div>
   </div>
 
-  <b> 
   <div class="right-section">
-    <div class="action-item"> 
-      <i class="bi bi-house"></i>
-      <svg xmlns="http://www.w3.org/2000/svg" width="17.5" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
-        <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
-      </svg>
+    <div class="action-item">
+      <a href="quickCart.html">
+        <i class="bi bi-house"></i>
+        <svg xmlns="http://www.w3.org/2000/svg" width="17.5" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+          <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
+        </svg>
         Home
+      </a>
     </div>
 
     <div class="action-item">
-      <a href="/Checkout.html" style="text-decoration: none;color: black">  
+      <a href="orders.html">
+        <div>
+          <div style ="font-weight: 100">Returns</div>
+          <div style ="font-weight: 600">& Orders</div>
+        </div>
+      </a>
+    </div>
+
+    <div class="action-item">
+      <a href="Checkout.html">  
         <i class="bi bi-cart3"></i>
         <svg xmlns="http://www.w3.org/2000/svg" width="17.5" height="16" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
           <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
@@ -55,11 +65,32 @@ export function renderHeader() {
         <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
       </svg>
     </div>
-  </div>
-  </b>
 
-`;
+    <div class="hamburger-container js-hamburger-container">
+      <img src="images/icons/hamburger-menu.png">
+    </div>
+
+    </div>
+    <div class="hidden js-hidden-container">
+      <div>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  `;
 
   document.querySelector('.js-header-container')
   .innerHTML = header;
+
+  document.querySelector('.js-hamburger-container')
+  .addEventListener('click', () => {
+    const hidden = document.querySelector('.js-hidden-container')
+    
+    if (hidden.classList.contains('hidden-container') ) {
+      hidden.classList.remove('hidden-container');
+    } else {
+      hidden.classList.add('hidden-container');
+    }
+  })
 }
