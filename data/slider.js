098ee5c61f0bf24c9c1c18f56b28009fed4b9640
Pankaj <pc620042@gmail.com>
2025-01-14 21:58:02 +0530

    Adjust the elements for responsiveness

--- data/slider.js
+++ data/slider.js
@@ -1,19 +1,10 @@
 export const slidesData = [{
-  image: 'images/pexels-photo-179909.webp',
-  alt: 'image #1',
-},{
-  image: 'images/homepage.webp',
-  alt: 'image #2',
-},{
-  image: 'images/thumbnail-1.webp',
-  alt: 'image #3',
-},{
-  image: 'images/slider1.jpg',
+  image: 'images/sliders/slider1.jpg',
   alt: 'image #4',
 },{
-  image: 'images/slider2.jpg',
+  image: 'images/sliders/slider2.jpg',
   alt: 'image #5',
 },{
-  image: 'images/slider3.webp',
+  image: 'images/sliders/slider3.webp',
   alt: 'image #6',
 }]
