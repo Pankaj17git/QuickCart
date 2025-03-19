1f716861e6c1c17b8f3a9aaa5929087f88b4bb10
Pankaj <panku620042@gmail.com>
2025-03-19 18:11:06 +0530

    Add login page to site

--- script/header.js
+++ script/header.js
@@ -74,17 +74,18 @@ export function renderHeader() {
       </div>
 
       <div class="action-item">
-        <a>
+        <a href="login.html">
           <i class="bi bi-person-circle"></i>
           <svg xmlns="http://www.w3.org/2000/svg"  width="36" height="70px" !important fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16" >
             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
             <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
           </svg>
         </a>
+        <p>fslfkj</p>
       </div>
 
       <div class="hamburger-container js-hamburger-container">
-        <img src="images/icons/hamburger-menu.png">
+          <img src="images/icons/hamburger-menu.png">
       </div>
 
       <div class="hidden js-hidden-container">
